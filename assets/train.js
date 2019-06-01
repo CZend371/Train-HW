f369e747e228a8d0ab299c6ee96c135328e7725b
Cristian371 <czend371@gmail.com>
2019-05-31 21:16:24 -0500

    added function to do arrival/mins away time, and css code

--- assets/train.js
+++ assets/train.js
@@ -16,6 +16,7 @@ var destination = null;
 var trainTime = null;
 var frequency = null;
 
+
 $("#add-train").click(function () {
     event.preventDefault();
 
@@ -27,6 +28,30 @@ $("#add-train").click(function () {
     console.log(trainTime);
     frequency = $("#frequency").val().trim();
     console.log(frequency);
+
+    // take input from first train time & frequency and write function to do the math
+    var trainTimeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
+    console.log(trainTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log(moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(trainTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % frequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var minUntilTrain = frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + minUntilTrain);
+
+    // Next Train
+    var nextTrain = moment().add(minUntilTrain, "minutes").format("hh:mm A");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
     database.ref().push({
         trainName: train,
@@ -40,9 +65,9 @@ $("#add-train").click(function () {
         $("<tr>").append(
             $("<td>").text(train),
             $("<td>").text(destination),
-            $("<td>").text(frequency)
-            // $("<td>").text(tArrival),
-            // $("<td>").text(tMinutes)
+            $("<td>").text(frequency),
+            $("<td>").text(nextTrain),
+            $("<td>").text(minUntilTrain)
         )
     );
     clearForm();
